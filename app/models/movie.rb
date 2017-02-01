75a0df2f32e83a081eadba4adc63f31eba330437
dkulpa <d.kulpa@selleo.com>
2017-02-01 13:52:38 +0100

    add validations for Movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,11 @@
 class Movie < ApplicationRecord
+  validates :title, :year, :time, :rating, presence: true
+  validates :year, inclusion: {
+    in: 1908..Time.now.year,
+    message: 'wrong year'
+  }
+  validates :rating, inclusion: {
+    in: 0..10,
+    message: 'wrong rating'
+  }
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,5 +1,86 @@
 require 'rails_helper'
 
-RSpec.describe Movie, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Movie do
+  describe 'validations' do
+    it 'enforces title to be present' do
+      movie = Movie.new
+      movie.valid?
+      expect(movie.errors.messages[:title]).to include("can't be blank")
+    end
+
+    it 'does not return errors if title is present' do
+      movie = Movie.new
+      movie.title = 'Tomb Raider'
+      movie.valid?
+      expect(movie.errors.messages[:title]).to_not include("can't be blank")
+    end
+
+    it 'enforces year to be present' do
+      movie = Movie.new
+      movie.valid?
+      expect(movie.errors.messages[:year]).to include("can't be blank")
+    end
+
+    it 'does not return errors if year is present' do
+      movie = Movie.new
+      movie.year = 2017
+      movie.valid?
+      expect(movie.errors.messages[:year]).to_not include("can't be blank")
+    end
+
+    it 'enforces time to be present' do
+      movie = Movie.new
+      movie.valid?
+      expect(movie.errors.messages[:time]).to include("can't be blank")
+    end
+
+    it 'does not return errors if time is present' do
+      movie = Movie.new
+      movie.time = 121
+      movie.valid?
+      expect(movie.errors.messages[:time]).to_not include("can't be blank")
+    end
+
+    it 'enforces rating to be present' do
+      movie = Movie.new
+      movie.valid?
+      expect(movie.errors.messages[:rating]).to include("can't be blank")
+    end
+
+    it 'does not return errors if rating is present' do
+      movie = Movie.new
+      movie.rating = 7.3
+      movie.valid?
+      expect(movie.errors.messages[:rating]).to_not include("can't be blank")
+    end
+
+    it 'enforces year to be between 1908 and current year' do
+      movie = Movie.new
+      movie1 = Movie.new
+      movie.year = 2018
+      movie1.year = 1722
+      movie.valid?
+      movie1.valid?
+      expect(movie.errors.messages[:year]).to include("wrong year")
+      expect(movie1.errors.messages[:year]).to include("wrong year")
+    end
+
+    it 'does not return errors if year is current year or past' do
+      movie = Movie.new
+      movie.year = Time.now.year
+      movie.valid?
+      expect(movie.errors.messages[:year]).to_not include("wrong year")
+    end
+
+    it 'enforces rating to be between 0 and 10' do
+      movie = Movie.new
+      movie1 = Movie.new
+      movie.rating = -2
+      movie1.rating = 12
+      movie.valid?
+      movie1.valid?
+      expect(movie.errors.messages[:rating]).to include("wrong rating")
+      expect(movie1.errors.messages[:rating]).to include("wrong rating")
+    end
+  end
 end
