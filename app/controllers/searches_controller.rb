9e0a362fb01457eb93edecbbd3c9cbb5e829e5da
dkulpa <d.kulpa@selleo.com>
2017-03-14 12:09:21 +0100

    refactor searches, prepare to remove

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,18 +5,12 @@ class SearchesController < ApplicationController
 
   def new
     @search = Search.new
-    @wanna_see_movies = PageParser.new('http://www.filmweb.pl/user/kulpadawid/films/wanna-see').parse_filmweb_titles
-    @my_movies_in_cineman = PageParser.new('http://www.cineman.pl/wszystkie').parse_cineman_movies(@wanna_see_movies)
   end
 
   def create
     @search = Search.new(search_params)
 
-    if @search.save
-      redirect_to '/searches/show'
-    else
-      render :new
-    end
+    redirect_to '/searches/show' if @search.save
   end
 
   private
